6f15d6bc11977214f94821a536f9eb9c1e7837a6
rana <ashikex49@gmail.com>
2021-10-09 21:35:47 +0600

    c_3

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import C_2 from './screens/c-2/C_2';
 import C_1 from './screens/c_1/C_1';
+import C_3 from './screens/c_3/C_3';
 
 const App = () => {
   return (
     <Router>
+      <Route path="/c-3" component={C_3} />
       <Route path="/c-1" component={C_1} />
       <Route path="/c-2" component={C_2} />
     </Router>
---
+++ src/screens/c_3/C_3.jsx
@@ -0,0 +1,42 @@
+import './c_3.scss';
+
+const C_3 = () => {
+  return (
+    <div className="c_3">
+      <div className="container">
+        <div className="card part-1">
+          <img src="/c_3/icon-sedans.svg" alt="" />
+          <div className="title">sedans</div>
+          <div className="desc">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cum
+            provident atque quaerat obcaecati earum rerum aut corporis
+            accusantium repellendus dicta.
+          </div>
+          <button>Learn more</button>
+        </div>
+        <div className="card part-2">
+          <img src="/c_3/icon-suvs.svg" alt="" />
+          <div className="title">sedans</div>
+          <div className="desc">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cum
+            provident atque quaerat obcaecati earum rerum aut corporis
+            accusantium repellendus dicta.
+          </div>
+          <button>Learn more</button>
+        </div>
+        <div className="card part-3">
+          <img src="/c_3/icon-luxury.svg" alt="" />
+          <div className="title">sedans</div>
+          <div className="desc">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cum
+            provident atque quaerat obcaecati earum rerum aut corporis
+            accusantium repellendus dicta.
+          </div>
+          <button>Learn more</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default C_3;
