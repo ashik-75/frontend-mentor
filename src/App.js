d281973f0dcf4f07c50b7b4252728a1135d798fe
rana <ashikex49@gmail.com>
2021-10-09 15:14:12 +0600

    c-2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import C_2 from './screens/c-2/C_2';
 import C_1 from './screens/c_1/C_1';
 
 const App = () => {
   return (
     <Router>
-      <Route path="/" component={C_1} />
+      <Route path="/c-1" component={C_1} />
+      <Route path="/c-2" component={C_2} />
     </Router>
   );
 };
---
+++ src/screens/c-2/C_2.jsx
@@ -0,0 +1,40 @@
+import './c_2.scss';
+
+const C_2 = () => {
+  return (
+    <div className="c_2">
+      <div className="card">
+        <div className="text-part">
+          <h1>
+            Get <span>insights</span> that help your business grow
+          </h1>
+          <div className="content">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sint, enim
+            dicta possimus consequuntur
+          </div>
+          <div className="stats">
+            <div className="stats-container">
+              <div className="stats-number">10k+</div>
+              <div className="stats-title">Companies</div>
+            </div>
+            <div className="stats-container">
+              <div className="stats-number">314</div>
+              <div className="stats-title">Template</div>
+            </div>
+            <div className="stats-container">
+              <div className="stats-number">12M+</div>
+              <div className="stats-title">Queries</div>
+            </div>
+          </div>
+        </div>
+        <div className="image-part">
+          <img className="img-mobile" src="/c_2/c-2-mobile.jpg" alt="" />
+          <img className="img-desktop" src="/c_2/c-2-desktop.jpg" alt="" />
+          <div className="over"></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default C_2;
